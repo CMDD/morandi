14bee1675599cf4ac6b7bfcd1c1252a900135feb
Desarrollo <desarrollo@iMac-de-John.local>
2019-04-24 16:12:13 -0500

    Agregado blog

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ Route::get('/nosotros', function () {return view('nosotros');});
 Route::get('/postventa', function () {return view('postventa');});
 Route::get('/proyecto/{project}', 'ProjectController@enVenta');
 Route::get('admin', function () {return view('admin.index');});
+Route::get('blog', function () {return view('blog');});
 
 Route::get('realizados/{project}','ProjectController@realizados');
