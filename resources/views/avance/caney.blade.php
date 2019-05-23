d1ffbe23aaa3dbce16d91a66ac70e4692b608c6f
John Alvaro <jhon54plex@hotmail.com>
2019-05-23 14:00:04 -0500

    Agregando avances caney

--- resources/views/avance/caney.blade.php
+++ resources/views/avance/caney.blade.php
@@ -1,13 +1,148 @@
 @extends('layouts.web')
+
 @section('content')
 
+    <div class="cover">
+        <div class="content">
+            <h1><span>Avance</span> de obra</h1>
+        </div>
+    </div>
+    <div class="container-fluid">
+
+        <div class="row logo">
+            <div class="col">
+                <img src="/images/caney/caney.png">
+            </div>
+        </div>
+
+        <div class="row gallery">
+            <div class="col-12 gallery-title">
+                <h3><span>Marzo</span> 2019</h3>
+            </div>
+            <div class="col-sm-8 gallery-1">
+                <div class="gallery-item he1"><a href="/images/caney/avances/2019m/IMG_4685.JPG"
+                        style="background-image: url(/images/caney/avances/2019m/IMG_4685.JPG);"></a></div>
+
+                <div class="gallery-item wi2l he2"><a href="/images/caney/avances/2019m/IMG_4687.JPG"
+                        style="background-image: url(/images/caney/avances/2019m/IMG_4687.JPG);"></a></div>
+
+                <div class="gallery-item wi2r he2"><a href="/images/caney/avances/2019m/IMG_4811.JPG"
+                        style="background-image: url(/images/caney/avances/2019m/IMG_4811.JPG);"></a></div>
+            </div>
+            <div class="col-sm-4 gallery-2">
+                <div class="gallery-item he2"><a href="/images/caney/avances/2019m/IMG_4673.JPG" style="background-image: url(/images/caney/avances/2019m/IMG_4673.JPG);"></a>
+                </div>
+                <div class="gallery-item he3"><a href="/images/caney/avances/2019m/IMG_4689.JPG" style="background-image: url(/images/caney/avances/2019m/IMG_4689.JPG);"></a>
+                </div>
+                <div class="gallery-item he4"><a href="/images/caney/avances/2019m/IMG_4685.JPG"
+                        style="background-image: url(/images/caney/avances/2019m/IMG_4685.JPG);"></a></div>
+            </div>
+            <!-- <div class="col-12 gallery-video">
+                <div class="video-embed">
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/lDU9mnCvuoQ" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div> -->
+            <!-- <div class="gallery-hide">
+                <a href="/images/bosque/galeria/10.jpg"></a>
+                <a href="/images/bosque/galeria/8.jpg"></a>
+                <a href="/images/bosque/galeria/1.jpg"></a>
+                <a href="/images/bosque/galeria/9.jpg"></a>
+            </div> -->
+        </div>
+
+        <div class="row gallery">
+            <div class="col-12 gallery-title">
+                <h3><span>Febrero</span> 2019</h3>
+            </div>
+            <div class="col-sm-8 gallery-1">
+                <div class="gallery-item he1"><a href="/images/caney/avances/2019f/IMG_3003.JPG"
+                        style="background-image: url(/images/caney/avances/2019f/IMG_3003.JPG);"></a></div>
+                <div class="gallery-item wi2l he2"><a href="/images/caney/avances/2019f/IMG_3002.JPG"
+                        style="background-image: url(/images/caney/avances/2019f/IMG_3002.JPG);"></a></div>
+
+                <div class="gallery-item wi2r he2"><a href="/images/caney/avances/2019f/IMG_3005.JPG"
+                        style="background-image: url(/images/caney/avances/2019f/IMG_3005.JPG);"></a></div>
+            </div>
+            <div class="col-sm-4 gallery-2">
+                <div class="gallery-item he2"><a href="/images/caney/avances/2019f/IMG_3004.JPG" style="background-image: url(/images/caney/avances/2019f/IMG_3004.JPG);"></a>
+                </div>
+                <div class="gallery-item he3"><a href="/images/caney/avances/2019f/IMG_3002.JPG" style="background-image: url(/images/caney/avances/2019f/IMG_3002.JPG);"></a>
+                </div>
+                <div class="gallery-item he4"><a href="/images/caney/avances/2019f/IMG_3003.JPG"
+                        style="background-image: url(/images/caney/avances/2019f/IMG_3003.JPG);"></a></div>
+            </div>
+           
+        </div>
+
+        <div class="row gallery">
+            <div class="col-12 gallery-title">
+                <h3><span>Agosto</span> 2018</h3>
+            </div>
+            <div class="col-sm-8 gallery-1">
+                <div class="gallery-item he1"><a href="/images/caney/avances/2018a/IMG_0132.JPG"
+                        style="background-image: url(/images/caney/avances/2018a/IMG_0132.JPG);"></a></div>
+                <div class="gallery-item wi2l he2"><a href="/images/caney/avances/2018a/IMG_0127.JPG"
+                        style="background-image: url(/images/caney/avances/2018a/IMG_0127.JPG);"></a></div>
+        
+                <div class="gallery-item wi2r he2"><a href="/images/caney/avances/2018a/IMG_0131.JPG"
+                        style="background-image: url(/images/caney/avances/2018a/IMG_0131.JPG);"></a></div>
+            </div>
+            <div class="col-sm-4 gallery-2">
+                <div class="gallery-item he2"><a href="/images/caney/avances/2018a/IMG_0130.JPG"
+                        style="background-image: url(/images/caney/avances/2018a/IMG_0130.JPG);"></a>
+                </div>
+                <div class="gallery-item he3"><a href="/images/caney/avances/2018a/IMG_7864.JPG"
+                        style="background-image: url(/images/caney/avances/2018a/IMG_7864.JPG);"></a>
+                </div>
+                <div class="gallery-item he4"><a href="/images/caney/avances/2018a/IMG_7865.JPG"
+                        style="background-image: url(/images/caney/avances/2018a/IMG_7865.JPG);"></a></div>
+            </div>
+            
+        </div>
+
+        <div class="row gallery">
+            <div class="col-12 gallery-title">
+                <h3><span>Octubre</span> 2017</h3>
+            </div>
+            <div class="col-sm-8 gallery-1">
+                <div class="gallery-item he1"><a href="/images/caney/avances/2017o/1.png"
+                        style="background-image: url(/images/caney/avances/2017o/1.png);"></a></div>
+                <div class="gallery-item wi2l he2"><a href="/images/caney/avances/2017o/PFJO1016.JPG"
+                        style="background-image: url(/images/caney/avances/2017o/PFJO1016.JPG);"></a></div>
+        
+                <div class="gallery-item wi2r he2"><a href="/images/caney/avances/2017o/IMG_6350.JPG"
+                        style="background-image: url(/images/caney/avances/2017o/IMG_6350.JPG);"></a></div>
+            </div>
+            <div class="col-sm-4 gallery-2">
+                <div class="gallery-item he2"><a href="/images/caney/avances/2017o/IMG_6354.JPG"
+                        style="background-image: url(/images/caney/avances/2017o/IMG_6354.JPG);"></a>
+                </div>
+                <div class="gallery-item he3"><a href="/images/caney/avances/2017o/PFJO1016.JPG"
+                        style="background-image: url(/images/caney/avances/2017o/PFJO1016.JPG);"></a>
+                </div>
+                <div class="gallery-item he4"><a href="/images/caney/avances/2017o/IMG_6350.JPG"
+                        style="background-image: url(/images/caney/avances/2017o/IMG_6350.JPG);"></a></div>
+            </div>
+            
+        </div>
+
+    </div>
+
+
     @include('layouts.footer')
+
 @stop
 
 @push('styles')
+    <link rel="stylesheet" href="../css/avance.css" type='text/css' media='all'>
 
+    <style>
+    .logo img{
+        /* width:10%; */
+    }
+    </style>
 @endpush
-@push('scripts')
 
+@push('scripts')
 
 @endpush
